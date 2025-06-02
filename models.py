ada19969ae5a85c29b2241fa6a7327d8ac356911
24f1000618-ds <24f1000618@ds.study.iitm.ac.in>
2025-06-02 18:24:11 +0530

    Milestone-VP DB-Relationship

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ import enum
 db = SQLAlchemy()
 
 class User(db.Model):
-    
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
@@ -14,14 +13,15 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     role = db.Column(db.String(35), nullable=False, default='user')
-    reserved = db.relationship('Reserve_Parking_Spot', backref='user', foreign_keys="[Reserve_Parking_Spot.user_id]")
+    reserve = db.relationship('Reserve_Parking_Spot', backref='user')
 
 class Parking_Lot(db.Model):
     __tablename__ = 'parking_lots'
 
     lot_id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(120), nullable=False)
-    pin_code = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+    base_price = db.Column(db.Float, nullable=False)
     price = db.Column(db.Float, nullable=False)
     max_spots = db.Column(db.Integer, nullable=False)
     spot = db.relationship('Parking_Spot', backref='lot')
@@ -34,16 +34,22 @@ class Parking_Spot(db.Model):
     __tablename__ = 'parking_spots'
 
     spot_id = db.Column(db.Integer, primary_key=True)
+    spot_no = db.Column(db.Integer, nullable=False)
     lot_id = db.Column(db.Integer, db.ForeignKey('parking_lots.lot_id'), nullable=False)
     status = db.Column(db.Enum(Spot_status), nullable=False, default=Spot_status.vacant)
-    reserve = db.relationship('Reserve_Parking_Spot', backref='spot', uselist=False)
+    reserved = db.relationship('Reserve_Parking_Spot', backref='spot')
     
 class Reserve_Parking_Spot(db.Model):
     __tablename__ = 'reserve_parking_spots'
 
     reserve_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spots.spot_id'), nullable=False, unique=True)
-    parking_time = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(db.String(80), db.ForeignKey('users.user_id'), nullable=False)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lots.lot_id'), nullable=False)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spots.spot_id'), nullable=False)
+    status = db.Column(db.Enum(Spot_status), nullable=False, default=Spot_status.vacant)
+    parking_time = db.Column(db.DateTime, server_default=db.func.now(), nullable=True)
     leaving_time = db.Column(db.DateTime, nullable=True)
+    duration = db.Column(db.Integer, nullable=True)
     cost = db.Column(db.Float, nullable=False)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    lot = db.relationship('Parking_Lot', backref='reservations')
